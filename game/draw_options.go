555abf95432c981ff2e726d0bdcf7fc324344db3
Jean-André Santoni <jean.andre.santoni@gmail.com>
2026-08-06 17:45:07 +0700

    Tighten RSM floor slab depth bias

--- game/draw_options.go
+++ game/draw_options.go
@@ -3,7 +3,10 @@ package game
 import "github.com/kivutar/goro/render"
 
 const groundDecalDepthBias = 1.0 / 32768.0
-const rsmModelDepthBias = groundDecalDepthBias
+
+// Some map RSMs are decorative floor slabs that intersect the GND by a few
+// hundredths of a world unit. Keep the nudge small, but stronger than decals.
+const rsmModelDepthBias = groundDecalDepthBias * 4
 
 func triangleDrawOptions(filter render.Filter, address render.Address) *render.DrawTrianglesOptions {
 	return &render.DrawTrianglesOptions{
